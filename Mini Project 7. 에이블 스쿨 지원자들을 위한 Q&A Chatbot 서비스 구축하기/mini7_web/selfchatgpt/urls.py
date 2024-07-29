fee106e8d5f857ef8f83d41d1643d3b2af91b6fb
YuBin-OuO <jyb424sch@gmail.com>
2024-07-30 01:51:10 +0900

    Update Mini Project 7

---
+++ Mini Project 7. 에이블 스쿨 지원자들을 위한 Q&A Chatbot 서비스 구축하기/mini7_web/selfchatgpt/urls.py
@@ -0,0 +1,12 @@
+# blog/urls.py
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+app_name = 'selfchatgpt'
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('chat', views.chat, name='chat'),
+
+]
